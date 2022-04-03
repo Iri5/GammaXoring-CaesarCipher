2c51b526ee96d5a84e1a47b0f155d4f7aee15b55
Iri5 <bovchurova-irina@rambler.ru>
2022-04-03 23:01:03 +0300

    removed unused functions

--- Coding/Input.cs
+++ Coding/Input.cs
@@ -24,43 +24,6 @@ namespace Coding
             }
             return dataByte;
         }
-        public static void KeyboardKeyNumber(ref string key)
-        {
-            Console.WriteLine("Введите ключ для шифра Цезаря - целое число");
-            int number = 0;
-            while (!int.TryParse(Console.ReadLine(), out number))
-            {
-                Console.WriteLine("Ошибка ввода! Введите целое число");
-            }
-            key = number.ToString();
-        }
-        public static void KeyboardStr(ref string data)
-        {
-            data = "";
-            while (data == "")
-            {
-                Console.WriteLine("Введите текст, длиной больше 0");
-                data = Console.ReadLine();
-            }
-        }
-        public static void KeyboardStrDecode(ref string data)
-        {
-            bool right = false;
-            while (right == false)
-            {
-                KeyboardStr(ref data);
-                Byte[] encryptedDataByte;
-                try
-                {
-                    encryptedDataByte = Input.SplitStringIntoBytes(data);
-                    right = true;
-                }
-                catch
-                {
-                    Console.WriteLine("Данные некорректны, попробуйте снова:");
-                    right = false;
-                }
-            }
-        }
+        
     }
 }
